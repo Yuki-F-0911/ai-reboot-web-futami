f158c2cc010d067ab65f50cb4734df5a7d3fe5ee
katiko1 <katiko3751@gmail.com>
2026-03-31 17:41:20 +0900

    redirect

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/corporate/ax1',
+        destination: '/corporate/ax1-0415',
+        permanent: false,
+      },
+      {
+        source: '/corporate/ax1-2',
+        destination: '/corporate/ax1-0415',
+        permanent: false,
+      },
+      {
+        source: '/corporate/ax1-special',
+        destination: '/corporate/ax1-0415',
+        permanent: false,
+      },
+    ]
+  },
   async rewrites() {
     return [
       {
